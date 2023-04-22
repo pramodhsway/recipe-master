553dbba29d15c13f891ba362ad35d7c92f2f94e5
Pramodh Gowda <pramodh.shivadas@sway-ai.com>
2023-04-22 14:26:50 -0400

    fix auth

--- src/Services/LoginService.js
+++ src/Services/LoginService.js
@@ -8,7 +8,7 @@ export const loginService = async (login) => {
   const loginInfo = await axios.post(LOGIN_URL, login);
   if (loginInfo.data.success) {
     localStorage.setItem("LoggedIn", JSON.stringify(loginInfo.data.userData));
-    localStorage.setItem("LoginToken", `${loginInfo.data.token}`);
+    localStorage.setItem("LoginToken", `Bearer ${loginInfo.data.token}`);
   }
   return loginInfo.data;
 };
